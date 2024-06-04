387a06113d40cba665f0fbb35d01947746e8c675
Christian Novalski <52799350+cnovalski1@users.noreply.github.com>
2024-06-04 13:04:16 -0400

    Example uploading a file.

---
+++ demo.py
@@ -0,0 +1,21 @@
+
+
+x = 5
+while x >= 1 :
+    print(x)
+    x = x - 1
+
+y = "Hello"
+
+try:
+    y = int(y)
+except:
+    y = -1
+
+print(y)
+
+def add(x, y) :
+    sum = x + y
+    print(sum)
+
+add(10, 20)
